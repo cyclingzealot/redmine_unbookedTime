106928d2b1c506bceea4f5f3ad06882aa7654562
Julien Lamarche <jlam@credil.org>
2015-02-16 16:12:41 -0500

    trying to get ajax to timesheet to work

--- Unbooked_time.user.js
+++ Unbooked_time.user.js
@@ -7,7 +7,7 @@
 // @grant       none
 // ==/UserScript==
   
-
+$("#header").append("Unbooked time GM script running...");
 
 //1. Calculate the amount of hours since the earliest ticket logging 
 var arrayOfTimesElemns = $("#activity > dl:nth-child(2) > dt.time-entry > span:nth-child(1)");
@@ -38,9 +38,38 @@ var timeEnd = $.now();
 
 var hourDiff = Math.round((timeEnd - timeStart)/1000/60/60*100)/100;             
 
-
 //2. Get the amount of hours logged today
+$("#header").append(new Error().lineNumber + "... ");
 
+var url = "https://code.credil.org/timesheet/report?timesheet%5Bdate_from%5D=2015-02-16&timesheet%5Bdate_to%5D=2015-02-16&timesheet%5Busers%5D%5B%5D=419 #time_entries > h2:nth-child(2)";
+var url = "https://code.credil.org/timesheet/report?timesheet[date_from]=2015-02-16&timesheet[date_to]=2015-02-16&timesheet[users][]=419";
+
+
+
+
+$("#header").append(new Error().lineNumber + "... ");
+
+$.ajax(
+    {
+        type: "GET",
+        url: url,
+        error: function(jqXHR, textStatus, errorThrown) {$("#header").append("Got " + errorThrown + " for " + url);},
+        success: function(msg)
+        {
+           $("#header").append(new Error().lineNumber + "... ");
+            $("#header").append(msg);
+
+        }
+    });
+
+var hoursSpent = 'None';
+
+
+$("#header").append(new Error().lineNumber + "... ");
+
+$("#header").append(hoursSpent);
+
+$("#header").append(new Error().lineNumber + "... ");
 
 
 var stringToAppend = '<div align="right">'+ hourDiff +'</div>';
